4ce6a86e465adf7ec48e92ae02f6f41f40f27b4c
Clemens Lange <clemens.lange@cern.ch>
2016-07-25 19:05:51 +0200

    make das_xsec.py work again

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # das_tools
+
+Requires CMSSW to be set up.
+
+## das_xsec.py
+
+Example query:
+```
+./das_xsec.py -d /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+```
+returns:
+
+```
+Will loop over datasets found to find MINIAODs and/or GEN
+Investigating /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+Finding parent of: /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM
+Found: /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM
+Finding parent of: /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring16DR80-PUSpring16_80X_mcRun2_asymptotic_2016_v3-v1/AODSIM
+Found: /QCD_HT1500to2000_BGenFilter_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer15wmLHEGS-MCRUN2_71_V1-v1/GEN-SIM
+Cross-section [pb]: 21.310000
+Filter efficiency: 1.000000 +/- 0.100000
+Matching efficiency: 0.153500 +/- 0.100000
+```
--- das_xsec.py
+++ das_xsec.py
@@ -6,94 +6,94 @@ import os
 import json
 import optparse
 
-def queryDAS(command):
-  subProcess = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-  output = subProcess.communicate()
-  print output
-  jsonOutput = json.loads(output[0])
-  if (jsonOutput["status"] != "ok"):
-    print "Failure in query: %s" %command
-    sys.exit()
-  return jsonOutput
+
+def queryDAS(queryString, verbose=False):
+
+    command = "das_client --query=\"{queryString}\" --format=json --limit=0".format(queryString=queryString)
+    if verbose:
+        print command
+    subProcess = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+    output = subProcess.communicate()
+    if verbose:
+        print output
+    jsonOutput = json.loads(output[0])
+    if (jsonOutput["status"] != "ok"):
+        print "Failure in query: %s" % command
+        sys.exit()
+    return jsonOutput
+
+
+def printXsec(xsecOutput):
+
+    # print last parameter in list in case there are several defined
+    genParams = xsecOutput["data"][0]["mcm"][0]["generator_parameters"][-1]
+    print "Cross-section [pb]: %f" % genParams["cross_section"]
+    print "Filter efficiency: %f +/- %f" % (genParams["filter_efficiency"], genParams["filter_efficiency_error"])
+    print "Matching efficiency: %f +/- %f" % (genParams["match_efficiency"], genParams["match_efficiency_error"])
+
 
 
 def main():
 
-  dataset = "/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/Fall13-POSTLS162_V1-v1/GEN-SIM"
-  dataset = "/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/*/*"
-  dataset = "/QCD*Pt*13TeV*/*/*"
-  dataset = "/QCDddbar_Pt-15to3000_TuneZ2star_Flat_13TeV_pythia6/RunIISpring15DR74-AsymptNoPU_MCRUN2_74_V9A-v1/MINIAODSIM"
-  
-  parser=optparse.OptionParser(usage="%prog -j jobOption.py")
-  parser.add_option("-d", "--dataset", dest="dataset",
-                      action="store", default=dataset,
+    # some example queries
+    # dataset = "/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/Fall13-POSTLS162_V1-v1/GEN-SIM"
+    # dataset = "/WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola/*/*"
+    # dataset = "/QCD*Pt*13TeV*/*/*"
+    # dataset = "/QCDddbar_Pt-15to3000_TuneZ2star_Flat_13TeV_pythia6/RunIISpring15DR74-AsymptNoPU_MCRUN2_74_V9A-v1/MINIAODSIM"
+
+    parser = optparse.OptionParser(usage="%prog -d DATASET")
+    parser.add_option("-d", "--dataset", dest="dataset",
+                      action="store",
                       help="dataset pattern to look for")
-  (options, args)=parser.parse_args()
-  dataset=options.dataset
-  
-  print "Looking for datasets of pattern: %s" %dataset
+    parser.add_option("-v", "--verbose", dest="verbose",
+                      action="store_true", default=False,
+                      help="print out all the query results")
+    (options, args) = parser.parse_args()
+    if not options.dataset:   # if dataset is not given
+    parser.error('dataset not given')
+    dataset = options.dataset
+    verbose = options.verbose
 
-  command = 'python das_client.py --query=\"dataset=%s\" --format=json --limit=0' %(dataset)
-  # print command
-  jsonOutput = queryDAS(command)
-  
-  
+    nResults = 1
+    if (("?" in dataset) or ("*" in dataset)):
+        print "Looking for datasets of pattern: %s" % dataset
+        queryString = 'dataset=%s' % (dataset)
+        jsonOutput = queryDAS(queryString, verbose)
 
-  # print jsonOutput
-  # print "----"
-  # print jsonOutput.keys()
-  # print "----"
-  # print jsonOutput["nresults"]
-  # print "----"
-  # print jsonOutput["data"][0]
-  # print "----"
-  # print jsonOutput["data"][0]["dataset"][0].keys()
-  # print "----"
-  # print jsonOutput["data"][0]["dataset"][0]["name"]
-  #
-  # print "---- loop ----"
-  
-  print "Found %i datasets" %(jsonOutput["nresults"])
-  
-  print "Will loop over datasets found to find MINIAODs and/or GEN"
-  
-  for i in range(jsonOutput["nresults"]):
-    thisDataset = jsonOutput["data"][i]["dataset"][0]["name"]
-    # print thisDataset
-    if ((thisDataset.find("MINIAOD") >= 0) or (thisDataset.find("/GEN") >= 0)):
-      print "Investigating %s" %thisDataset
-      eventsCommand = 'python das_client.py --query=\"dataset=%s\" --format=json --limit=0' %(thisDataset)
-      eventsOutput = queryDAS(eventsCommand)
-      # print eventsOutput["data"][0]["dataset"]
-      print "Number of events: %i" %eventsOutput["data"][0]["dataset"][0]["nevents"]
-      print "Finding parent:"
-      parentDataset = thisDataset
-      maxSteps = 5
-      currentStep = 0
-      while ((parentDataset.find("/GEN") < 0) and (currentStep < maxSteps)):
-        parentCommand = 'python das_client.py --query=\"parent dataset=%s\" --format=json --limit=0' %(parentDataset)
-        parentOutput = queryDAS(parentCommand)
-        print parentOutput["data"][0]["parent"][0]["name"]
-        parentDataset = parentOutput["data"][0]["parent"][0]["name"]
-        currentStep += 1
-      if (currentStep == maxSteps):
-        print "ERROR: Couldn't find parent of type GEN for %s" %thisDataset
-        continue
-      xsecCommand = 'python das_client.py --query=\"mcm dataset=%s | grep mcm.generator_parameters\" --format=json --limit=0' %(parentDataset)
-      xsecOutput = queryDAS(xsecCommand)
-      # print xsecOutput["data"][0]["mcm"][0]["generator_parameters"]
-      print "Cross-section [pb]: %f" %xsecOutput["data"][0]["mcm"][0]["generator_parameters"][0]["cross_section"]
-      print "Filter efficiency: %f +/- %f" %(xsecOutput["data"][0]["mcm"][0]["generator_parameters"][0]["filter_efficiency"], xsecOutput["data"][0]["mcm"][0]["generator_parameters"][0]["filter_efficiency_error"])
-      print "Matching efficiency: %f +/- %f" %(xsecOutput["data"][0]["mcm"][0]["generator_parameters"][0]["match_efficiency"], xsecOutput["data"][0]["mcm"][0]["generator_parameters"][0]["match_efficiency_error"])
-      
-      
-  
-  
-  
-  # number of events from MINIAOD
-  # xsec for GEN-SIM
+        nResults = jsonOutput["nresults"]
+        print "Found %i datasets" % (nResults)
 
-  # os.system('python das_client.py --query=\"parent dataset=%s\" --format=json' %(dataset))
+    print "Will loop over datasets found to find MINIAODs and/or GEN"
+
+    for i in range(nResults):
+        thisDataset = dataset
+        if (nResults > 1):
+            thisDataset = jsonOutput["data"][i]["dataset"][0]["name"]
+
+        if ((thisDataset.find("MINIAOD") >= 0) or (thisDataset.find("/GEN") >= 0)):
+            print "Investigating %s" % thisDataset
+            parentDataset = thisDataset
+            maxSteps = 5
+            currentStep = 0
+            while ((parentDataset.find("/GEN") < 0) and (currentStep < maxSteps)):
+                print "Finding parent of:", parentDataset
+                queryString = 'parent dataset=%s' % (parentDataset)
+                parentOutput = queryDAS(queryString, verbose)
+                print "Found:", parentOutput["data"][0]["parent"][0]["name"]
+                parentDataset = parentOutput["data"][0]["parent"][0]["name"]
+                currentStep += 1
+            if (currentStep == maxSteps):
+                print "ERROR: Couldn't find parent of type GEN for %s" % thisDataset
+                continue
+            queryString = 'mcm dataset=%s | grep mcm.generator_parameters' % (parentDataset)
+            xsecOutput = queryDAS(queryString, verbose)
+            printXsec(xsecOutput)
+
+    # number of events from MINIAOD
+    # xsec for GEN-SIM
+
+    # os.system('python das_client.py --query=\"parent dataset=%s\" --format=json' %(dataset))
 
 if __name__ == "__main__":
-  main()
+    main()
